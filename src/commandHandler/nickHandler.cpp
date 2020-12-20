e4d54a7aab19c47ad542c35e4a1db2d92e86e06b
daeungkim <daeungkim93@gmail.com>
2020-12-20 12:46:28 +0900

    update:코드 정돈
    <description>
    코드 정돈
    <issue-number>

--- src/commandHandler/nickHandler.cpp
+++ src/commandHandler/nickHandler.cpp
@@ -1,23 +1,31 @@
 #include "Server.hpp"
 
-static bool			checkStr(std::string str, std::string::iterator iterator)
+static bool			isValidNickname(const Message &message)
 {
-	if (str.find(*iterator) != std::string::npos)
-		return (true);
-	return (false);
+	for (size_t i = 0; i < message.getParameter(0).length(); i++)
+	{
+		if (i == 0 && !isValidFormat(std::string(LETTER) + std::string(SPECIAL), message.getParameter(0)[i]))
+			return false;
+		else if (!isValidFormat(std::string(LETTER) + std::string(SPECIAL) + std::string(DIGIT), message.getParameter(0)[i]))
+			return false;
+	}
+		return true;
 }
 
-static bool			checkNickForm(std::string nick)
+static void			setNick(Client *client, const Message &message,
+					std::map<std::string, Client> &sendClients,
+					std::map<std::string, Client *> &clientList)
 {
-	std::string::iterator iterator;
-
-	iterator = nick.begin();
-	if (!checkStr(std::string(LETTER) + std::string(SPECIAL), iterator))
-		return (false);
-	while (++iterator != nick.end())
-		if (!checkStr(std::string(LETTER) + std::string(SPECIAL) + std::string(DIGIT), iterator))
-			return (false);
-	return (true);
+	if (client->getInfo(ORIGINNICK) == "")
+		client->setInfo(ORIGINNICK, message.getParameter(0));
+	else
+	{
+		sendClients.erase(client->getInfo(CURRENTNICK));
+		clientList.erase(client->getInfo(CURRENTNICK));
+	}
+	client->setInfo(CURRENTNICK, message.getParameter(0));
+	sendClients[message.getParameter(0)] = *client;
+	clientList[message.getParameter(0)] = client;
 }
 
 int					Server::nickHandler(const Message &message, Client *client)
@@ -31,7 +39,7 @@ int					Server::nickHandler(const Message &message, Client *client)
 			return ((this->*(this->replies[ERR_NONICKNAMEGIVEN]))(message, client));
 		if (1 < message.getParameters().size())
 			return ((this->*(this->replies[ERR_NEEDMOREPARAMS]))(message, client));
-		if (!checkNickForm(message.getParameter(0)) || 9 < message.getParameter(0).length())
+		if (!isValidNickname(message) || 9 < message.getParameter(0).length())
 			return ((this->*(this->replies[ERR_ERRONEUSNICKNAME]))(message, client));
 		if (!client->getIsAuthorized())
 		{
@@ -40,16 +48,7 @@ int					Server::nickHandler(const Message &message, Client *client)
 		}
 		if (this->sendClients.find(message.getParameter(0)) != this->sendClients.end())
 			return ((this->*(this->replies[ERR_NICKNAMEINUSE]))(message, client));
-		if (client->getInfo(ORIGINNICK) == "")
-			client->setInfo(ORIGINNICK, message.getParameter(0));
-		else
-		{
-			this->sendClients.erase(client->getInfo(CURRENTNICK));
-			this->clientList.erase(client->getInfo(CURRENTNICK));
-		}
-		client->setInfo(CURRENTNICK, message.getParameter(0));
-		this->sendClients[message.getParameter(0)] = *client;
-		this->clientList[message.getParameter(0)] = client;
+		setNick(client, message, this->sendClients, this->clientList);
 		if (client->getInfo(USERNAME) == "")
 		{
 			this->sendMessage(Message("", message.getParameter(0), ":Nick registered"), client);
