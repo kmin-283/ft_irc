d3fa087288e1008b6d767348eee846099f71ef27
stevenkim18 <11882@naver.com>
2021-01-20 03:31:37 +0900

    basic privmsg send

--- src/commandHandler/privmsgHandler.cpp
+++ src/commandHandler/privmsgHandler.cpp
@@ -1,10 +1,44 @@
 #include "Server.hpp"
 
+/**
+ * seunkim -> song 에게 보냄
+ * 
+ * :seunkim!~6@localhost PRIVMSG song :123
+ * :보낸nick!~username@
+ * 
+ */
+
+std::string     getClientPrefix(Client *client)
+{
+    std::string prefix;
+
+    prefix += client->getInfo(NICK);
+    prefix += std::string("!~");
+    prefix += client->getInfo(USERNAME);
+    prefix += std::string("@");
+    prefix += client->getInfo(HOSTNAME);
+
+    return (prefix);
+}
+
 int     Server::privmsgHandler(const Message &message, Client *client)
 {
+    Client *targetClient;
+
     client->setCurrentCommand("PRIVMSG");
-    (void)message;
-    std::cout << "client num = " << this->clientList.size() << std::endl;
-    std::cout << "client nick = " << client->getInfo(NICK) << std::endl;
+    
+    // clientList에서 이름 찾기
+    if (this->clientList.find(message.getParameter(0)) != this->clientList.end())
+    {
+        targetClient = this->clientList[message.getParameter(0)];
+        std::cout << "find! nick = " << message.getParameter(0) << std::endl;
+        this->sendMessage(Message(":" + getClientPrefix(client), "PRIVMSG", message.getParameter(0) + " :" + message.getParameter(1)), targetClient);
+        // std::cout << "hostname = " << targetClient->getInfo(HOSTNAME) << std::endl;
+        // std::cout << "nick = " << targetClient->getInfo(NICK) << std::endl;
+        // std::cout << "address = " << targetClient->getInfo(ADDRESS) << std::endl;
+        // std::cout << "username = " << targetClient->getInfo(USERNAME) << std::endl;
+        // std::cout << "realname = " << targetClient->getInfo(REALNAME) << std::endl;
+    }
+
     return (CONNECT);
 }
